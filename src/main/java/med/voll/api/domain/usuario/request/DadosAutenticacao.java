8e9d63c4073967bd8b54a772d693a55d1454fa10
Allyson Abreu <allyson.cz@hotmail.com>
2024-06-04 12:15:40 -0300

    Altera e implementa autenticação via tokenJWT.

---
+++ src/main/java/med/voll/api/domain/usuario/request/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.usuario.request;
+
+public record DadosAutenticacao(String login, String senha) {
+}
---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = recuperarToken(request);
+
+        if(token != null) {
+            var subject = tokenService.getSubjectId(token);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var token = request.getHeader("Authorization");
+
+        if (token != null) {
+            return token.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
